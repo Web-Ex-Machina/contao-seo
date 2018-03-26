0e342766692d3ee0781461ebb8cc1a3cb7185a62
Lupus <julien@webexmachina.fr>
2018-03-26 15:32:49 +0200

    + Really add the meta fields to tl_page.

---
+++ dca/tl_page.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * Module SEO for Contao Open Source CMS
+ *
+ * Copyright (c) 2018 Web ex Machina
+ *
+ * @author Web ex Machina <https://www.webexmachina.fr>
+ */
+
+/**
+ * List of fields added :
+ * Classical meta : canonical,metaImage
+ * OpenGraph (http://ogp.me/) : og:title, og:type, og:image, og:url, og:description
+ * Twitter (https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/summary) : twitter:card, twitter:site, twitter:title, twitter:description, twitter:image, twitter:image:alt
+ */
+
+/**
+ * Update Regular & Root palette
+ */
+$GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace
+(
+	'description;',
+	'metaCanonical,metaImage,description;{opengraph_legend:hide},overrideOGTags;{twitter_legend:hide},overrideTwitterTags;',
+	$GLOBALS['TL_DCA']['tl_page']['palettes']['regular']
+);
+$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace
+(
+	'pageTitle',
+	'pageTitle,metaCanonical,metaImage;{opengraph_legend:hide},overrideOGTags;{twitter_legend:hide},overrideTwitterTags',
+	$GLOBALS['TL_DCA']['tl_page']['palettes']['root']
+);
+
+/**
+ * Update Selector palettes
+ */
+$GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'overrideOGTags';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'overrideTwitterTags';
+
+/**
+ * Update Subpalettes
+ */
+$GLOBALS['TL_DCA']['tl_page']['subpalettes']['overrideOGTags'] = 'metaOGTitle,metaOGType,metaOGImage,metaOGUrl,metaOGDescription';
+$GLOBALS['TL_DCA']['tl_page']['subpalettes']['overrideTwitterTags'] = 'metaTwitterCard,metaTwitterSite,metaTwitterTitle,metaTwitterDescription,metaTwitterImage,metaTwitterImageAlt';
+
+/**
+ * Add Fields
+ */
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaCanonical'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaCanonical'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('rgxp'=>'url', 'maxlength'=>255, 'tl_class'=>'w100 clr'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaImage'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaImage'],
+	'exclude'                 => true,
+	'inputType'               => 'fileTree',
+	'eval'                    => array('filesOnly'=>true, 'fieldType'=>'radio', 'extensions'=>Config::get('validImageTypes'), 'tl_class'=>'clr'),
+	'sql'                     => "binary(16) NULL"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['overrideOGTags'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['overrideOGTags'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'eval'                    => array('submitOnChange'=>true),
+	'sql'                     => "char(1) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['overrideTwitterTags'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['overrideTwitterTags'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'eval'                    => array('submitOnChange'=>true),
+	'sql'                     => "char(1) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaOGTitle'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaOGTitle'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'decodeEntities'=>true, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaOGType'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaOGType'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'decodeEntities'=>true, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaOGImage'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaOGImage'],
+	'exclude'                 => true,
+	'inputType'               => 'fileTree',
+	'eval'                    => array('filesOnly'=>true, 'fieldType'=>'radio', 'extensions'=>Config::get('validImageTypes'), 'tl_class'=>'clr'),
+	'sql'                     => "binary(16) NULL"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaOGUrl'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaOGUrl'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('rgxp'=>'url', 'maxlength'=>255, 'tl_class'=>'w100'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaOGDescription'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaOGDescription'],
+	'exclude'                 => true,
+	'inputType'               => 'textarea',
+	'eval'                    => array('style'=>'height:60px', 'decodeEntities'=>true, 'tl_class'=>'clr'),
+	'sql'                     => "text NULL"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaTwitterCard'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaTwitterCard'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'decodeEntities'=>true, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaTwitterSite'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaTwitterSite'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'decodeEntities'=>true, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaTwitterTitle'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaTwitterTitle'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'decodeEntities'=>true, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaTwitterDescription'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaTwitterDescription'],
+	'exclude'                 => true,
+	'inputType'               => 'textarea',
+	'eval'                    => array('style'=>'height:60px', 'decodeEntities'=>true, 'tl_class'=>'clr'),
+	'sql'                     => "text NULL"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaTwitterImage'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaTwitterImage'],
+	'exclude'                 => true,
+	'inputType'               => 'fileTree',
+	'eval'                    => array('filesOnly'=>true, 'fieldType'=>'radio', 'extensions'=>Config::get('validImageTypes'), 'tl_class'=>'clr'),
+	'sql'                     => "binary(16) NULL"
+);
+$GLOBALS['TL_DCA']['tl_page']['fields']['metaTwitterImageAlt'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['metaTwitterImageAlt'],
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'decodeEntities'=>true, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
