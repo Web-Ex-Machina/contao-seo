82462e66bc50c64c30d79ee5e1e6e0f08192f396
Lupus <julien@webexmachina.fr>
2018-03-26 15:28:14 +0200

    + Add Meta fields to tl_page

---
+++ config/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Module SEO for Contao Open Source CMS
+ *
+ * Copyright (c) 2018 Web ex Machina
+ *
+ * @author Web ex Machina <https://www.webexmachina.fr>
+ */
---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Module SEO for Contao Open Source CMS
+ *
+ * Copyright (c) 2018 Web ex Machina
+ *
+ * @author Web ex Machina <https://www.webexmachina.fr>
+ */
+
+if(TL_MODE == "FE")
+{
+	//$GLOBALS['TL_HOOKS']['generatePage'][] = array('WEM\SEO\Hooks', 'applySEORules');
+}
